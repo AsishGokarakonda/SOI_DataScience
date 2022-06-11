986a7c80c5870bb209a941a78492c5e1d41e27fd
karthik murakonda <karthik.murakonda14@gmail.com>
2022-06-11 22:55:54 +0530

    updated flask web app to load the saved model
    - allows to download result.csv file

--- app.py
+++ app.py
@@ -1,10 +1,44 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 import pandas as pd
 from keras.models import Sequential
 from keras.layers import Dense
 import tensorflow as tf
 
+model = tf.keras.models.load_model('files/model')
 app = Flask(__name__)
+
+def string_to_onehot(arr):  # Convert text labels to one-hot encoding for categorical classification
+  int_arr = []
+
+  for x in arr:
+    if x=='AFP':
+      int_arr.append([1,0,0,0])
+    
+    elif x=='NTP':
+      int_arr.append([0,1,0,0])
+    
+    elif x=='PC':
+      int_arr.append([0,0,1,0])
+      
+    elif x=='UNK':
+      int_arr.append([0,0,0,1])
+  
+  return int_arr
+
+def onehot_to_string(int_arr):  # Convert back one-hot to text
+  res = []
+
+  for x in int_arr:
+    if max(x) == x[0]:
+      res.append('AFP')
+    elif max(x) == x[1]:
+      res.append('NTP')
+    elif max(x) == x[2]:
+      res.append('PC')
+    elif max(x) == x[3]:
+      res.append('UNK')
+  return res
+
 
 @app.route('/',methods=['GET'])
 
@@ -15,80 +49,32 @@ def hello_world():
 @app.route('/',methods=['POST'])
 
 def predict ():
-    imagefile = request.files['imagefile']
-    image_path = "./files/" + imagefile.filename
-    imagefile.save(image_path)
-    data = pd.read_csv(image_path)
+    csvfile = request.files['imagefile']
+    csv_path = "./files/" + csvfile.filename
+    csvfile.save(csv_path)
+
+    data = pd.read_csv(csv_path)
     data.drop('tce_rogue_flag', inplace=True, axis=1)
     data.drop('tce_insol', inplace=True, axis=1)
     data.drop('tce_insol_err', inplace=True, axis=1)
+    data.drop('kepid', inplace=True, axis=1)
 
-    y = data['av_training_set']
-    x = data.drop('av_training_set', axis=1)
+    x_test = data.drop('av_training_set', axis=1)
+    y_test = pd.DataFrame(string_to_onehot(data['av_training_set']))
+    result = model.predict(x_test)
+    result = onehot_to_string(result)
+    result_csv = pd.DataFrame(result, index=None, columns=['av_training_set'])
+    actual_result = pd.DataFrame(data['av_training_set'], columns=['av_training_set'])
+    # compare the predicted and actual results
+    result_csv.columns = ['av_training_set']
+    result_csv.to_csv('files/result.csv',index=False)
+    return render_template('index.html', results_csv='/result.csv')
 
-
-    # print(y)
-
-
-    x_train = x[:13000].copy()
-    y_train = y[:13000].copy()
-    x_test = x[13000:].copy()
-    y_test = y[13000:].copy()
-    def string_to_onehot(arr):
-        int_arr = []
-
-        for x in arr:
-            if x=='AFP':
-                int_arr.append([1,0,0,0])
-            
-            elif x=='NTP':
-                int_arr.append([0,1,0,0])
-            
-            elif x=='PC':
-                int_arr.append([0,0,1,0])
-            
-            elif x=='UNK':
-                int_arr.append([0,0,0,1])
-        
-        return int_arr
-
-    def onehot_to_string(int_arr):
-        arr = []
-
-        for x in int_arr:
-            if x==[1,0,0,0]:
-                int_arr.append('AFP')
-            
-            elif x==[0,1,0,0]:
-                int_arr.append('NTP')
-            
-            elif x==[0,0,1,0]:
-                int_arr.append('PC')
-            
-            elif x==[0,0,0,1]:
-                int_arr.append('UNK')
-    y_train = string_to_onehot(y_train)
-    y_test = string_to_onehot(y_test)
-    model = Sequential()
-    model.add(Dense(36, input_dim=23, activation='relu'))
-    model.add(Dense(20, activation='relu'))
-    model.add(Dense(20, activation='relu'))
-    model.add(Dense(20, activation='relu'))
-    model.add(Dense(4, activation='softmax'))
-
-    model.summary()
-    model.compile(optimizer='adam',loss = 'categorical_crossentropy', metrics=['accuracy'])
-    y_train = pd.DataFrame(y_train)
-    # print(y_train)
-    model.fit(x_train, y_train, epochs=200, batch_size=500)
-    y_test = pd.DataFrame(y_test)
-    _, accuracy = model.evaluate(x_test,y_test)    #testing
-    print("Model accuracy: %.2f"% (accuracy*100))
-    acc = accuracy*100
-
-
-    return render_template('index.html',accuracy=acc)
-
+@app.route('/files/<path:path>', methods=['GET'])
+def down_file(path):
+    path = "./files/" + path
+    file = open(path, 'rb')
+    return send_file(file, as_attachment=True, attachment_filename='result.csv')
     
 if __name__ == '__main__':
     app.run(port=3000,debug=True)
